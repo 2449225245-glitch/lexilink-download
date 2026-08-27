53031a217b9a09dc23a231632c97a569831f8bf9
2449225245-glitch <2449225245@qq.com>
2026-08-27 17:11:34 +0800

    release: update QR download target for v1.0.0

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 (function(){
-  var apkUrl = 'https://github.com/2449225245-glitch/lexilink-download/releases/download/v0.7.6.1/LexiLink-v0.7.6.1.apk';
+  var apkUrl = 'https://github.com/2449225245-glitch/lexilink-download/releases/download/v1.0.0/LexiLink-v1.0.0.apk';
   var qr = document.getElementById('qr');
   qr.src = 'https://api.qrserver.com/v1/create-qr-code/?size=320x320&margin=8&data=' + encodeURIComponent(apkUrl);
 })();
